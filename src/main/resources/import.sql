6a9fc74d5e8072eb12ceceee970b4d0e57d080fe
Edvaldo Oliveira <ed.ferreiraoliveira@gmail.com>
2026-02-13 21:12:15 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$UEkcZpSwGR4vWDK7iBa8g.lnXc1igunCYEU.NOYZGtX6c0O83aS66');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$UEkcZpSwGR4vWDK7iBa8g.lnXc1igunCYEU.NOYZGtX6c0O83aS66');
 
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
