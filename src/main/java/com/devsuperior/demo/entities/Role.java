d2d2a5524719ffb5b4c9a71b8c0af0e8faf89902
Edvaldo Oliveira <ed.ferreiraoliveira@gmail.com>
2026-02-18 08:24:30 -0300

    Spring security checklist

--- src/main/java/com/devsuperior/demo/entities/Role.java
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,12 +1,20 @@
 package com.devsuperior.demo.entities;
 
-import jakarta.persistence.*;
-
 import java.util.Objects;
 
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,6 +36,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
@@ -37,15 +46,19 @@ public class Role {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Role role = (Role) o;
-        return Objects.equals(authority, role.authority);
+    public int hashCode() {
+        return Objects.hash(authority);
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(authority);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        return Objects.equals(authority, other.authority);
     }
 }
